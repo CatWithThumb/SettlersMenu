9f3d3288ad2a5e3ed241a592ef91de9667ee0974
CatWithThumb <tomraine03@gmail.com>
2023-04-26 11:07:22 +0100

    second commit, gitignore

---
+++ Assets/Scripts/CpuNumScript.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CpuNumScript : MonoBehaviour
+{
+    public int NumCpuPlayers = 0;
+
+    public void HandleInputData(int val) //handles TMPdropdown menu values
+    {
+        if (val == 0)
+        {
+            NumCpuPlayers = 0;
+            Debug.Log("CPU = "+NumCpuPlayers);
+        }
+
+        if (val == 1)
+        {
+            NumCpuPlayers = 1;
+            Debug.Log("CPU = "+NumCpuPlayers);
+        }
+
+        if (val == 2)
+        {
+            NumCpuPlayers = 2;
+            Debug.Log("CPU = "+NumCpuPlayers);
+        }
+
+        if (val == 3)
+        {
+            NumCpuPlayers = 3;
+            Debug.Log("CPU = "+NumCpuPlayers);
+        }
+    }
+}
